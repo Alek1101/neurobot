9f9dc49fc9529f8d6811d2117da85ef23efaf627
Alek1101 <kevinalek10@gmail.com>
2024-03-01 21:47:08 +0300

    3.0 Добавлены логи, полностью завершена функциональность бота, добавлены файлы: Readme.md, requirements.txt

--- neurobot/bot.py
+++ neurobot/bot.py
@@ -1,15 +1,40 @@
 import telebot
-from API_TOKEN import TOKEN
+from config import TOKEN, TOKENS_LENGTH
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from neuro import ask, save_progress, load_progress
+from neuro import ask, save_progress, load_progress, count_tokens
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%d.%m.%Y, %H:%M:%S',
+    filename='log_file.txt',
+    filemode='w'
+                    )
 
 bot = telebot.TeleBot(TOKEN)
 
 check = False
 
 
+@bot.message_handler(content_types=['audio', 'photo', 'voice', 'video', 'document', 'location', 'contact', 'sticker'])
+def text_filter(m):
+    user = m.from_user.first_name
+    if type(m.from_user.last_name) is str:
+        user += m.from_user.last_name
+    user_id = m.from_user.id
+    bot.send_message(m.chat.id, 'Бот воспринимает только сообщения...')
+    logging.debug(f'Попытка взломать систему, пользователь {user}, id = {user_id}.')
+
+
 @bot.message_handler(commands=['start'])
 def start(m):
+    logging.debug('Команда /start активирована')
+    user = m.from_user.first_name
+    if type(m.from_user.last_name) is str:
+        user += m.from_user.last_name
+    user_id = m.from_user.id
+    logging.info(f'Активация бота пользователем {user}, id = {user_id}')
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add(KeyboardButton('/solve_task'))
     bot.send_message(m.chat.id, 'Привет! Я - Telegram-бот с подключённой нейросетью, созданный, чтобы отвечать'
@@ -21,6 +46,7 @@ def start(m):
 
 @bot.message_handler(commands=['solve_task'])
 def instruction(m):
+    logging.debug('Команда /solve_task активирована')
     bot.send_message(m.chat.id, 'Я отвечаю на вопросы на тему программирования, \nнапиши свой вопрос кратко '
                                 'и точно и укажи язык программирования, о котором идёт речь, '
                                 '\nа также язык, на котором '
@@ -30,26 +56,40 @@ def instruction(m):
 
 
 def question_treatment(m):
-    id = m.from_user.id
+    user_id = m.from_user.id
     global check
     check = False
-    user_id = m.from_user.id
+    user = m.from_user.first_name
+    if type(m.from_user.last_name) is str:
+        user += m.from_user.last_name
     if m.content_type != 'text':
-        bot.send_message(user_id, text='Отправь промт текстовым сообщением')
+        logging.debug(f'Попытка отправить особое медиа, пользователь {user}, id = {user_id}')
+        bot.send_message(m.chat.id, 'Отправь промт текстовым сообщением')
         bot.register_next_step_handler(m, question_treatment)
         return
     if m.text[0] == '/':
+        logging.debug(f'Попытка обойти систему, пользователь {user} с id = {user_id}')
         bot.send_message(m.chat.id, 'Сначала задай свой вопрос.')
         bot.register_next_step_handler(m, question_treatment)
         return
     user_prompt = m.text
-    save_progress(id, user_prompt)
-    bot.send_message(m.chat.id, 'Промт принят!')
+    if count_tokens(user_prompt) >= TOKENS_LENGTH:
+        logging.debug('Превышено допустимое количество символов.')
+        logging.info(f'Пользователь {user}, id = {user_id} превысил допустимое количество символов.')
+        bot.send_message(m.chat.id, 'Количество символов превышает допустимый порог.\n'
+                                    'Пожалуйста, повтори вопрос.')
+        bot.register_next_step_handler(m, question_treatment)
+        return
+    logging.info(f'Пользователь {user}, id = {user_id} задал вопрос:\n< {user_prompt} >')
+    save_progress(user_id, user_prompt)
+    bot.send_message(m.chat.id, 'Промт принят! \nВремя ожидания до 5 минут.')
     check = True
-    # bot.send_chat_action(m.chat.id, 'анализирует...')
-    bot.send_message(m.chat.id, ask(user_prompt))
-    # Сообщение от GPT
-    #     bot.send_message(m.chat.id, '')
+    try:
+        bot.send_message(m.chat.id, ask(user_prompt))
+        logging.debug('Генерация ответа завершена')
+    except:
+        bot.send_message(m.chat.id, 'Возникла проблема при генерации ответа...\nМы уже работаем над этим.')
+        logging.warning('Ошибка при генерации ответа нейросетью')
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add('/continue', '/solve_task')
     bot.send_message(m.chat.id, '/continue для продолжения, \n'
@@ -58,22 +98,61 @@ def question_treatment(m):
 
 @bot.message_handler(commands=['continue'])
 def question_continuation(m):
-    id = m.from_user.id
+    logging.debug('Команда /continue активирована')
+    user_id = m.from_user.id
+    user = m.from_user.first_name
+    if type(m.from_user.last_name) is str:
+        user += m.from_user.last_name
     if check:
-        # bot.send_chat_action(m.chat.id, 'усиленно думает...')
         bot.send_message(m.chat.id, 'Продолжение ответа...')
-        bot.send_message(m.chat.id, ask('Продолжи:' + load_progress(id)))
+        # try:
+        if not load_progress(id) == 'error':
+            answer = ask('Продолжи:' + load_progress(id))
+        else:
+            answer = 'Не удалось получить ответ от нейросети'
+            logging.warning('Ошибка открытия файла')
+        bot.send_message(m.chat.id, answer)
+        logging.debug('Генерация ответа завершена')
+        # except:
+        #     bot.send_message(m.chat.id, 'Возникла проблема при генерации ответа...\nМы уже работаем над этим.')
+        #     logging.warning('Ошибка при генерации ответа нейросетью')
         markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         markup.add('/continue', '/solve_task')
         bot.send_message(m.chat.id, '/continue для продолжения, \n'
                                     '/solve_task - для нового вопроса.', reply_markup=markup)
     else:
+        logging.debug(f'Попытка обойти систему, пользователь {user}, id = {user_id}')
         markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         markup.add('/solve_task')
         bot.send_message(m.chat.id, 'Сначала задай вопрос с помощью команды /solve_task.', reply_markup=markup)
 
 
-#     Продолжение ответа
+@bot.message_handler(commands=['debug'])
+def logs(m):
+    f = open('log_file.txt', 'r')
+    try:
+        bot.send_document(m.chat.id, f)
+    except:
+        logging.warning('Не удалось отправить документ с логами')
+        bot.send_message(m.chat.id, 'Не удалось отправить документ с логами')
+        if m.from_user.first_name != 'Alek':
+            print('Функция /debug была использована сторонним пользователем.')
+            logging.info('Команда /debug активирована')
+    f.close()
+
+
+@bot.message_handler(content_types=['text'])
+def other(m):
+    user_id = m.from_user.id
+    user = m.from_user.first_name
+    if type(m.from_user.last_name) is str:
+        user += m.from_user.last_name
+    logging.debug(f'Сработала функция сортировки простого текста, пользователь {user}, id = {user_id}.')
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.add(KeyboardButton('/solve_task'))
+    markup.add(KeyboardButton('/start'))
+    bot.send_message(m.chat.id, 'Чтобы взаимодействовать со мной, используй команды '
+                                '/solve_task \nили /start.', reply_markup=markup)
 
 
 bot.infinity_polling()
--- neurobot/neuro.py
+++ neurobot/neuro.py
@@ -1,4 +1,6 @@
 import requests
+from transformers import AutoTokenizer
+from config import TOKENS_LENGTH, temperature, HEADERS, GPT_LOCAL_URL
 
 system_content = ('Ты - дружелюбный помощник для ответов на вопросы по программированию. '
                   'Определи, на каком языке программирования написан код, и дай подробный ответ на поставленный вопрос'
@@ -22,19 +24,24 @@ def ask(user_message: str):
         task = prompt
     messages.append({'role': 'user', 'content': task})
     response = requests.post(
-        'http://158.160.135.104:1234/v1/chat/completions',
-        headers={'Content-Type': 'application/json'},
+        url=GPT_LOCAL_URL,
+        headers=HEADERS,
         json={
             'messages': messages,
-            'temperature': 1.2,
-            'max_tokens': 2048
+            'temperature': temperature,
+            'max_tokens': TOKENS_LENGTH
         }
     )
-    try:
+    if response.status_code == 200:
         result = response.json()['choices'][0]['message']['content']
         return result, True
-    except:
-        return 'Произошла ошибка при генерации ответа', False
+    else:
+        return response.json(), False
+
+
+def count_tokens(prompt: str):
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")  # название модели
+    return len(tokenizer.encode(prompt))
 
 
 import json
@@ -45,7 +52,7 @@ def save_progress(user_id, question_number):
     try:
         with open('users_data.json', 'r') as file:
             progress = json.load(file)
-        progress[str(user_id)] = question_number.encode('utf8')
+        progress[str(user_id)] = question_number
         with open('users_data.json', 'w') as file:
             json.dump(cur_progress, file)
     except:
@@ -53,13 +60,10 @@ def save_progress(user_id, question_number):
             json.dump(cur_progress, file)
 
 
-# Загрузка прогресса пользователя из json
-
-
 def load_progress(user_id):
     try:
         with open('users_data.json', 'r') as file:
             progress = json.load(file)
-            return progress.get(str(user_id)).decode('utf8')
-    except FileNotFoundError:
-        return None
+            return progress.get(str(user_id))
+    except:
+        return 'error'
