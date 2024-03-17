12aa8a7ae50b24757edeb9bd42577f1c3fd6db91
Alek1101 <kevinalek10@gmail.com>
2024-03-17 16:19:52 +0300

    2.0 Расширенная версия первого Telegram-бота с нейросетью Написаны основные Telegram-функции и обращения к gpt-модели

--- data_save project/bot.py
+++ data_save project/bot.py
@@ -1,11 +1,11 @@
 import logging
 
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from config import TOKEN, languages
+from gpt import count_tokens, ask_gpt
 
 bot = telebot.TeleBot(TOKEN)
-
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -14,6 +14,8 @@ logging.basicConfig(
     filename='logs_file.txt',
     filemode='w'
 )
+
+user_data = []
 
 
 def language_(text: str):
@@ -33,12 +35,11 @@ def create_keyboard(options):
 @bot.message_handler(commands=['debug'])
 def debug(m):
     try:
-        with open ('logs_file.txt', 'r') as f:
+        with open('logs_file.txt', 'r') as f:
             bot.send_document(m.chat.id, f)
     except:
         bot.send_message(m.chat.id, 'Не удалось прислать файл с логами')
         logging.warning('Не удалось отправить документ с логами')
-
 
 
 @bot.message_handler(commands=['start'])
@@ -56,17 +57,58 @@ def names(m):
 
 
 @bot.message_handler(commands=['ask'])
-def ask_gpt(m):
+def ask_bot(m):
     bot.send_message(m.chat.id, 'Задайте свой вопрос об одном из следующих языков программирования:',
                      reply_markup=create_keyboard(languages))
-    bot.register_next_step_handler(m, gpt_answer)
+    bot.register_next_step_handler(m, language_choice)
+
+
+def language_choice(m):
+    if not language_(m.text):
+        bot.send_message(m.chat.id, 'Выберите один из представленных языков программирования:',
+                         reply_markup=create_keyboard(languages))
+        bot.register_next_step_handler(m, language_choice)
+    else:
+        language = m.text
+        user_data.append(language)
+        bot.send_message(m.chat.id, 'Выберите свой уровень: ',
+                         reply_markup=create_keyboard(['начинающий', 'продвинутый']))
+        bot.register_next_step_handler(m, level_choice)
+
+
+def level_choice(m):
+    if m.text not in ['начинающий', 'продвинутый']:
+        bot.send_message(m.chat.id, 'Выберите свой уровень: ',
+                         reply_markup=create_keyboard(['начинающий', 'продвинутый']))
+        bot.register_next_step_handler(m, level_choice)
+    else:
+        level = m.text
+        user_data.append(level)
+        bot.send_message(m.chat.id, 'Задайте свой вопрос')
+        bot.register_next_step_handler(m, gpt_answer)
 
 
 def gpt_answer(m):
-    if not language_(m.text):
-        bot.send_message(m.chat.id,'Выберите один из представленных языков программирования:',
-                         reply_markup=create_keyboard(languages))
-        return
+    task = m.text
+    if not count_tokens(task):
+        bot.send_message(m.chat.id, 'Слишком длинный вопрос.\nПопробуйте ещё раз.')
+        bot.register_next_step_handler(m, gpt_answer)
+    else:
+        user_data.append(task)
+        bot.send_message(m.chat.id, 'Вопрос принят, ожидайте ответа...')
+        try:
+            bot.send_message(m.chat.id, ask_gpt(user_data[0], user_data[1], user_data[2]),
+                             reply_markup=create_keyboard(['завершить ответ', 'продолжить ответ']))
+        except:
+            bot.send_message(m.chat.id, 'Не удалось получить ответа от нейросети.\nПопробуйте ещё раз.')
+            logging.warning('Ошибка при генерации ответа нейросетью.')
+            bot.register_next_step_handler(m, gpt_answer)
+
+
+def gpt_continue(m):
+    if m.text == 'завершить ответ':
+        bot.send_message(m.chat.id, 'Ответ завершён')
+    # elif m.text == 'продолжить ответ':
 
 
 
--- data_save project/gpt.py
+++ data_save project/gpt.py
@@ -1,3 +1,35 @@
 import requests
 from transformers import AutoTokenizer
 from config import TOKENS_LENGTH, HEADERS, GPT_LOCAL_URL, temperature
+
+
+def ask_gpt(language: str, level: str, question: str, assistant_content=None):
+    system_content = (f'Ты - дружелюбный помощник для ответов на вопросы по языку программирования {language}.'
+                      f'Дай ответ на заданный вопрос. Уровень понимания пользователем языка - {level}.'
+                      f'Учитывай уровень пользователя при ответе.')
+    messages = [{'role': 'system', 'content': system_content}, {'role': 'user', 'content': question},
+                {'role': 'assistant', 'content': assistant_content}]
+    response = requests.post(
+        url=GPT_LOCAL_URL,
+        headers=HEADERS,
+        json={
+            'messages': messages,
+            'temperature': temperature,
+            'max_tokens': TOKENS_LENGTH
+        }
+    )
+    if response.status_code == 200:
+        result = response.json()['choices'][0]['message']['content']
+        return result, True
+    else:
+        return response.json(), False
+
+
+def count_tokens(prompt: str):
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")  # название модели
+    if len(tokenizer.encode(prompt)) > TOKENS_LENGTH:
+        return False
+    else:
+        return True
+
+
